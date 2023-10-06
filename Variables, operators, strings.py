bf5632074199a5012bc671456e4df85a5b30252d
Codrina Lisaru <94629883+codrinaignat@users.noreply.github.com>
2023-10-06 13:31:33 +0300

    Add files via upload

---
+++ Variables, operators, strings.py
@@ -0,0 +1,71 @@
+#Variables, operators, strings
+
+#Ex1
+
+a = 15
+b = 30
+sum = a + b
+div = sum/2
+print(a, b, div)
+
+#Ex2
+
+c = 10
+d = 12
+print(c, d)
+suma = c + d
+print(suma)
+diff = c - d
+print(diff)
+prod = c * d
+print(prod)
+div = c/d
+print(div)
+rem = c%d
+print(rem)
+
+#Ex3
+
+name = "Codrina"
+print(name)
+
+#Ex4
+
+mancare1, mancare2, mancare3 = "Pizza", "Sarmale", "Musaca"
+
+print(mancare1)
+print(mancare2)
+print(mancare3)
+
+#Ex5
+print("Hello\n" *10)
+
+#Ex6
+
+myName, myAge = "Codrina", 26
+print(myName, myAge)
+
+#Ex7
+
+String1 = "Ana are mere "
+String2 = "si Dan are pere"
+String3 = String1 + String2
+print(String3)
+
+#Ex8
+
+myString = "Avion"
+print(myString[3])
+
+#Ex9
+
+myNewString = "Alabalaportocala"
+print(myNewString[0:5])
+
+#Ex10
+
+myVariable = "Alabalaportocala"
+
+for elements in myVariable:
+    print(elements, end=' ')
+
